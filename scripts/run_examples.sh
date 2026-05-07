bf2bff88a8803c95f9caac8fe6ff31153be59718
Ryohei Sasaki <rsasaki0109@gmail.com>
2026-05-08 02:28:51 +0900

    add visual odometry prior tracking example

--- scripts/run_examples.sh
+++ scripts/run_examples.sh
@@ -17,6 +17,7 @@ online_slam_loop_candidate_dummy
 track_sequence_dummy
 track_sequence_with_extractor_dummy
 track_sequence_with_gnss_prior
+track_sequence_with_visual_odometry_prior
 visual_odometry_prior_dummy
 "
 
